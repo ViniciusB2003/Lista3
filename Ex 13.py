8debf58fd8fba5eac135ef59f83d597bfa88edaa
Vinícius Barbosa <viniciusbarbosa500@outlook.com>
2022-06-20 16:37:44 -0400

    Finalizei atividade 13 falta apenas 18

---
+++ Ex 13.py
@@ -0,0 +1,6 @@
+def sequencia_escada(n):
+    n1 = []
+    for x in range(n+1):
+        n1.append(x)
+        print(n1)
+l = sequencia_escada(int(input('Digite o último número da sequência desejada: ')))
---
+++ Ex 18.py
@@ -0,0 +1,30 @@
+def data_cal(dia,mes,ano):
+    while (dia <= 0 or dia >= 31) and (mes <= 0 and mes > 12) and (ano <= 0):
+        return print('NULL')
+    if mes == 1:
+        mes = 'Janeiro'
+    elif mes == 2:
+        mes = 'Fevereiro'
+    elif mes == 3:
+        mes = 'Março'
+    elif mes == 4:
+        mes = 'Abril'
+    elif mes == 5:
+        mes = 'Maio:'
+    elif mes == 6:
+        mes = 'Junho'
+    elif mes == 7:
+        mes = 'Julho'
+    elif mes == 8:
+        mes = 'Agosto'
+    elif mes == 9:
+        mes = 'Setembro'
+    elif mes == 10:
+        mes = 'Outubro'
+    elif mes == 11:
+        mes = 'Novembro'
+    elif mes == 12:
+        mes = 'Dezembro'
+    dataf = dia, ' de ', mes, ' de ', ano
+    return dataf
+datai = int(input('Digite a data:',)
